1763d755354123e9f045cd5c0f8a19b1353242ec
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-03-11 09:53:07 -0300

    bootstrap page

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,7 +1,7 @@
 const transformDataToElement = require("./renderCards");
 const tempData = require("./tempData");
 
-console.log(tempData);
+// console.log(tempData);
 
 
 $.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
