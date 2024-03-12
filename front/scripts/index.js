e08d249ecbf300a193f60037338eea1f5f316404
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-03-12 08:29:25 -0300

    implementing axios library to make a https request

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,9 +1,22 @@
 const transformDataToElement = require("./renderCards");
 const tempData = require("./tempData");
+const axios = require("axios");
 
+//? IMPORTING DATA MOVIE JSON 
 // console.log(tempData);
 
+//? GET REQUEST USIN CDN TO THE JQUERY
+// $.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
+//   transformDataToElement(data);
+// });
 
-$.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
-  transformDataToElement(data);
-});
+//? GET REQUEST USIN AXIOS LIBRARY
+axios
+  .get("https://students-api.up.railway.app/movies")
+  .then((response) => {
+    const data = response.data;
+    transformDataToElement(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
